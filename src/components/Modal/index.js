2451b0dcba18fe56c6d9727933bb213d47301755
ThandarTun22 <thandartun.tt2022@gmail.com>
2023-04-25 14:28:34 +0630

    global vs component stylesheet

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -4,10 +4,12 @@ import ReactDom from 'react-dom'
 
 export default function Modal({children}) {
   return (
-    ReactDom.createPortal(<div className="modal-backdrop">
+    ReactDom.createPortal(<div className='modal-component'>
+      <div className="modal-backdrop">
     <div className="modal">
         {children}
     </div>
-</div>, document.getElementById('modal'))
+</div>
+    </div>, document.getElementById('modal'))
   )
 }
